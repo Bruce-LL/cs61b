c2cdcac01930ad2803363641cdf1af6126b9fd7b
Xian Wei Long <xianweilong@Xians-MacBook-Air.local>
2022-04-08 23:36:06 -0400

    complete first part of lab1

---
+++ lab1/Hellonumber.java
@@ -0,0 +1,8 @@
+public class Hellonumbers {
+    public static void main(String[] args){
+        int index = 10;
+        for(int i = 0; i < index; i ++){
+            System.out.println(i);
+        }
+    }
+}
